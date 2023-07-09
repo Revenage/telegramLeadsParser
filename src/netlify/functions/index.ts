fe0bb05bb2a501e1afd77a5c43d57220cac68c21
revenage <rv@firstsport.io>
2023-07-09 20:52:40 +0300

    test api

--- src/netlify/functions/index.ts
+++ src/netlify/functions/index.ts
@@ -1,11 +1,6 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
 
-const api = express();
+import app from "../../server";
 
-const router = Router();
-
-router.get("/hello", (req, res) => res.send("Hello World!"));
-api.use("/api/", router);
-
-export const handler = serverless(api);
+export const handler = serverless(app);
